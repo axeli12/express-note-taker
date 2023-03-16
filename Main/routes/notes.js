4868742e53edff23b9588684204387489e88107f
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-03-16 17:56:30 -0600

    notes.js getting notes

--- Main/routes/notes.js
+++ Main/routes/notes.js
@@ -3,6 +3,14 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const { json } = require=('express');
+const fs = require('fs');
 
+
+notes.get('/notes', (req, res) => {
+    fs.readFile(path.resolve(__dirname, '..', 'db/db.json'), 'utf8', (err, data) => {
+        if (err) console.log(err);
+        return res.json(JSON.parse(data));
+    });
+});
 
 module.exports = notes;
