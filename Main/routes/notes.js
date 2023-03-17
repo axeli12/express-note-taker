80e31ada222dd7b833ee9e7b7e72f8fb490ed71a
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-03-16 18:09:51 -0600

    adding gets to server.js

--- Main/routes/notes.js
+++ Main/routes/notes.js
@@ -2,7 +2,7 @@ const notes = require('express').Router();
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
-const { json } = require=('express');
+const { json } = require =('express');
 const fs = require('fs');
 
 
@@ -12,5 +12,30 @@ notes.get('/notes', (req, res) => {
         return res.json(JSON.parse(data));
     });
 });
+
+notes.post('/notes', (req, res) => {
+    if (req.body) {
+        const { title,text } = req.body;
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        fs.readFile(path.resolve(__dirname, '..', 'db/db.json'), 'utf8', (err, data) => {
+            if (err) console.log(err);
+            console.log(data);
+            let newData = JSON.parse(data)
+            console.log('New Data', newData)
+
+            newData.push(newNote)
+
+            let diffData = JSON.stringify(newData)
+
+            fs.writeFile(path.resolve(__dirname, '..', 'db/db.json'), diffData, (err) => {
+                return res.json();
+            })
+        })
+    }
+})
 
 module.exports = notes;
--- Main/server.js
+++ Main/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path')
 
 // Initialize the app and create a port
 const app = express();
@@ -16,4 +15,12 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 // Start the server on the port
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
